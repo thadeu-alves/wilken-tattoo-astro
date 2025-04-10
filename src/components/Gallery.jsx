3854f9a7be23e908176da804a0f51b5250234c14
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-04-10 12:54:12 -0300

    update images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,32 +2,32 @@ export function Gallery() {
     return (
         <section className="grid grid-cols-3 px-6 gap-x-2 gap-y-4 ">
             <img
-                src="./public/tattoos/tattoo1.jpeg"
+                src="./tattoos/tattoo1.jpeg"
                 alt="Tattoo 1"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
             <img
-                src="./public/tattoos/tattoo2.jpeg"
+                src="./tattoos/tattoo2.jpeg"
                 alt="Tattoo 2"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
             <img
-                src="./public/tattoos/tattoo3.jpeg"
+                src="./tattoos/tattoo3.jpeg"
                 alt="Tattoo 3"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
             <img
-                src="./public/tattoos/tattoo4.jpeg"
+                src="./tattoos/tattoo4.jpeg"
                 alt="Tattoo 4"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
             <img
-                src="./public/tattoos/tattoo5.jpeg"
+                src="./tattoos/tattoo5.jpeg"
                 alt="Tattoo 5"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
             <img
-                src="./public/tattoos/tattoo6.jpeg"
+                src="./tattoos/tattoo6.jpeg"
                 alt="Tattoo 6"
                 className="w-full h-40 object-cover rounded-xl border-2 border-yellow-700"
             />
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ export function Profile() {
         <div className="w-screen flex flex-col justify-center items-center py-6 border-b-yellow-700 border-b gap-y-2">
             <img
                 className="w-32 rounded-full object-cover"
-                src="./public/profilewilken.jpg"
+                src="./profilewilken.jpg"
                 alt="profile image"
             />
             <h1 className="text-xl font-bold mt-4">
@@ -11,7 +11,7 @@ export function Profile() {
             </h1>
             <p className="font-light text-yellow-500 flex items-center gap-2">
                 <img
-                    src="./public/insta.svg"
+                    src="./insta.svg"
                     alt="insta logo"
                     className="w-6"
                 />
